d16fee96e85eca2485e79413db16f08f343bddff
enjoysun <1570819807@qq.com>
2017-01-19 15:20:38 +0800

    线程和主线程关系，以及子线程资源销毁作用范围等，Task引用和返回值写法级异步回调写法，结束后再次执行任务写法实例

---
+++ TaskDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskDemo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Task<int> t = Task<int>.Run<int>(() =>
+            {
+                Console.WriteLine("task线程启动");
+                return 1;
+            });
+            //Console.WriteLine(t.Result);
+            //Console.WriteLine(t.IsCompleted);
+            //ContinueWith（）让该后台线程继续执行新的任务
+            Task<string> t2=t.ContinueWith<string>(c =>
+            {
+                return "hello";
+            });
+            //做到异步回调
+            t.GetAwaiter().OnCompleted(() => {
+                Console.WriteLine(t.Result+" word");
+                Console.WriteLine(t2.Result+" word");
+            });
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ThreadDemo/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ThreadDemo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //SemaphoreSlim:负责协调线程，限制资源被访问的线程数量
+            Console.WriteLine("启动");
+
+
+            Thread t = new Thread(c =>
+            {
+                using (FileStream fs = new FileStream(@"D:\我的mvc\C#异步\Synccallback\ThredDemo.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite))
+                {
+
+                    using (StreamWriter stw = new StreamWriter(fs))
+                    {
+                        //Console.WriteLine("t线程启动");
+                        stw.WriteLine("t线程启动");
+                        //Thread.Sleep(10000);
+                        for (int i = 0; i <= 100; i++)
+                        {
+                            stw.WriteLine(i);
+                            //Console.WriteLine(i);
+                        }
+
+                    }
+                }
+            }) { IsBackground = true };
+            t.Start();
+            Thread.Sleep(5000);
+            Console.WriteLine("结束");
+        }
+    }
+}
